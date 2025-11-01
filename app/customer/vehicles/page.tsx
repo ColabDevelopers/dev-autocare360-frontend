c50381052f7032cc1fd63f56938f287c9a7d8b76
SankalyaTharindi <sankalya.tharindi@gmail.com>
2025-11-01 22:13:04 +0530

    Revert "Fix vehicle list import and resolve remaining issues"
    This reverts commit 390177c14973caf6c67ff1270211ec929b11970e.

--- app/customer/vehicles/page.tsx
+++ app/customer/vehicles/page.tsx
@@ -1,9 +1,14 @@
 "use client"; // needed if using hooks like useState/useEffect
 
 import React from "react";
+import { useVehicles } from "@/hooks/useVehicles";
 import VehiclesList from "@/components/vehicle/VehiclesList";
 
 export default function CustomerVehiclesPage() {
+  const { vehicles, loading } = useVehicles();
+
+  if (loading) return <div>Loading...</div>;
+
   return (
     <div className="p-4">
       <VehiclesList />
